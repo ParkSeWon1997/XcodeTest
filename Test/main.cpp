c7481223e4f877411785d8ae3e81b3dcfc614c11
ParkSeWon1997 <jusin142@ruu.kr>
2025-03-25 20:29:11 +0900

    window와 Mac연결

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,16 +1,51 @@
-//
-//  main.cpp
-//  Test
-//
-//  Created by 박세원 on 3/25/25.
-//
-
 #include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, char* argv[])
+{
     
-    //테스트
+    const char* serverIP[1];
+    int hSocket;
+    char message[30] = {};
+    struct sockaddr_in servAddr;
+
+    // 소켓 생성
+    hSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (hSocket == -1) {
+        perror("[Fail] socket()");
+        return -1;
+    }
+
+    // 서버 주소 설정
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr("000.000.000.000");  // 로컬 테스트
+    servAddr.sin_port = htons(8888);
+
+    // 연결 시도
+    if (connect(hSocket, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1) {
+        perror("[Fail] connect()");
+        close(hSocket);
+        return -1;
+    }
+
+    // 데이터 수신
+    int strLen = recv(hSocket, message, sizeof(message) - 1, 0);
+    if (strLen == -1) {
+        perror("[Fail] recv()");
+        close(hSocket);
+        return -1;
+    }
+
+    message[strLen] = 0;
+    printf("Message from server: %s\n", message);
+
+    close(hSocket);
     return 0;
 }
